09c182b700eb4b14b86b28bdfc2449977fe41140
liaojimlin <liaojimlin@gmail.com>
2019-01-31 10:40:58 +0800

    Java and Kotlin varable, datd type

---
+++ src/main/java/com/jason/Hello.java
@@ -0,0 +1,21 @@
+package com.jason;
+
+public class Hello {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+      Person p = new Person();
+      p.hello();
+      int age = 19;
+      Integer age2 = 19;
+      char c = 'A';
+      Character c2 = 'c';
+      byte b = 120;
+      float weight = 66.5f;
+      double height = 1.7;
+      boolean adult = ture;
+      boolean enroll = false;
+      String name = "Jason";
+}
+
+
+
---
+++ src/main/kotlin/com/jason/Hello.kt
@@ -0,0 +1,18 @@
+package com.jason
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+    val h = Human()
+    var age = 19
+    age = 20;
+    var weight = 66.5f
+    val name: String
+    name = "Jason"
+
+}
+
+class Human{
+     fun hello(){
+         println("Hello kotlin")
+     }
+}
